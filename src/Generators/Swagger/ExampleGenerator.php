45b821485491651a310d5e38c80beb8a425820d7
Humberto Cuadra <beto.cuadra811@gmail.com>
2024-06-25 11:58:43 -0600

    Fixing issues with custom user functions and faker attribute mapper.

--- src/Generators/Swagger/ExampleGenerator.php
+++ src/Generators/Swagger/ExampleGenerator.php
@@ -13,10 +13,12 @@ class ExampleGenerator
     // Variable will only need to contain any of these keys so the function is called
 
     private $faker;
+    private $customFunctions;
 
     public function __construct()
     {
         $this->faker = fake();
+        $this->customFunctions = config('langsys-generator.custom_functions');
     }
 
 
@@ -27,22 +29,15 @@ class ExampleGenerator
 
         $function = $this->_getExampleFunction($name);
 
-        // If $function is an array, handle it as a custom function
-        if ($function && is_array($function)) {
-            [$class, $method] = $function;
-            return app($class)->$method(...array_values($arguments));
-        }
-
         // If $function is a string, handle it as a method on this class or a Faker method
-        if ($function && is_string($function)) {
-            if (method_exists($this, $function)) {
-                return $this->$function(...array_values($arguments));
-            }
+        if (is_string($function) && isset($this->customFunctions[$function])) {
+            [$class] = $this->customFunctions[$function];
+            return app($class)->$function(...array_values($arguments));
         }
 
         try {
             unset($arguments['type']);
-            $camelCaseName = Str::camel($name);
+            $camelCaseName = Str::camel($function);
 
             $example = $this->faker->$camelCaseName(...$arguments);
             $example = is_array($example) ? array_pop($example) : $example;
@@ -53,14 +48,8 @@ class ExampleGenerator
         }
     }
 
-    private function _getExampleFunction(string $name): callable|bool|array
+    private function _getExampleFunction(string $name): string
     {
-        // Check for custom function definitions first
-        $customFunctions = config('langsys-generator.custom_functions');
-        if (array_key_exists($name, $customFunctions)) {
-            return $customFunctions[$name];  // Directly use the custom function if specified
-        }
-
         // Fallback to default function handling
         if (str_starts_with($name, self::FAKER_FUNCTION_PREFIX)) {
             $functionName = str_replace(self::FAKER_FUNCTION_PREFIX, '', $name);
@@ -69,13 +58,14 @@ class ExampleGenerator
 
         // Check the attribute mapping for default or extended configurations
         $mapping = config('langsys-generator.faker_attribute_mapper');
+
         foreach ($mapping as $hint => $function) {
             if (str_contains($name, $hint)) {
                 return $function;
             }
         }
 
-        return false;
+        return $name;
     }
 
 
